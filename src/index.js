29a7502d4fbced754d17c71d7c7c7080d553e96b
Brian Gerdes <brian.gerdes@protonmail.com>
2023-09-16 23:24:38 -0700

    updated images

--- src/index.js
+++ src/index.js
@@ -1,36 +1,62 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const books = [
+	{
+		title: 'Dad Jokes',
+		author: 'Jimmy Niro',
+		img: 'https://m.media-amazon.com/images/I/713toGwMnqL._SL1500_.jpg',
+	},
+	{
+		title: 'Atomic Habits',
+		author: 'James Clear',
+		img: 'https://m.media-amazon.com/images/I/81bGKUa1e0L._SL1500_.jpg',
+	},
+];
 
 const BookList = () => {
 	return (
 		<section className='booklist'>
-			<Book />
-			<Book />
-			<Book />
-			<Book />
+			{books.map((book) => {
+				return <Book {...book} key={book.id} />;
+			})}
 		</section>
-	)
-}
+	);
+};
 
-const Book = () => {
+const handleEvents = () => {
+	const handleFormInput = () => {
+		console.log('Handle form input');
+	};
+	const handleButtonClick = () => {
+		alert('Handle form input');
+	};
+	return (
+		<section>
+			<form action=''>
+				<h2>Typical Form</h2>
+				<input
+					type='text'
+					name='example'
+					onChange={handleFormInput}
+					style={{ margin: '1rem 0' }}
+				/>
+			</form>
+			<button onClick={handleButtonClick}>Click Me</button>
+		</section>
+	);
+};
+
+const Book = ({ title, author, img }) => {
 	return (
 		<article className='book'>
-			<Image />
-			<Title />
-			<Author />
+			<img src={img} alt={title} />
+			<h2>{title}</h2>
+			<h4>{author}</h4>
 		</article>
-	)
-}
+	);
+};
 
-const Image = () => (
-	<img
-		src='https://m.media-amazon.com/images/I/4143s1BpV2L._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
-		alt='Dad Jokes'
-	/>
-)
-const Title = () => <h2>Dad Jokes</h2>
-const Author = () => <h4>Jimmy Niro</h4>
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<BookList />)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<BookList />);
